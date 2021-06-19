6ac96005f5d18ad9fd65df7aaa317fbfc34b30b7
AvinashSubhash <avinashsubhash19@gmail.com>
2021-06-19 21:37:47 +0530

    added_basic_structure

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import numpy as np
 
-def InitializeParameters(dimension):
+X = np.random.randn(3,1)
+Y = np.zeros((2,1))
+
+def layer_sizes(X,Y):
+    n_0 = X.shape[0]
+    n_1 = 4
+    n_2 = Y.shape[0]
+    return [n_0,n_1,n_2]
+
+
+def InitializeParameters(dimension=layer_sizes(X,Y)):
     input_size = dimension[0]
     hidden_layer_size = dimension[1]
     output_size = dimension[2]
 
-    w1 = np.random.randn((hidden_layer_size,input_size))*0.001
+    w1 = np.random.randn(hidden_layer_size,input_size)*0.001
     b1 = np.zeros((hidden_layer_size,1))
-    w2 = np.random.randn((output_size,hidden_layer_size))*0.001
+    w2 = np.random.randn(output_size,hidden_layer_size)*0.001
     b2 = np.zeros((output_size,1))
 
     data = {"w1": w1 ,"w2": w2 ,"b1": b1 ,"b2": b2}
+    data['X']=X
+    data['Y'] = Y
+    CheckDimension(data)
     return data
 
 def ForwardPropagation(data,input_data,i):
@@ -20,7 +33,15 @@ def ForwardPropagation(data,input_data,i):
     data['a'+str(i)] = a
     return data
 
+def CheckDimension(data):
+    print("Input Shape: ",data['X'].shape)
+    for i in range(1,3):
+        print("Layer ",i,": ",data['w'+str(i)].shape,end='')
+        print("  Bias ",i,": ",data['b'+str(i)].shape)
+    print("Output Shape: ",data['Y'].shape)
+
 def BackwardPropagation(data,i):
+    pass
 
-
-
+if __name__ == "__main__":
+    InitializeParameters()
